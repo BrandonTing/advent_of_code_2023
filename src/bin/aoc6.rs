f6cd196f6016555c264363415f9cedf2957a2474
Brandon Ting <brandon1221t@gmail.com>
2023-12-15 09:11:25 +0800

    day 6 part 2

--- src/bin/aoc6.rs
+++ src/bin/aoc6.rs
@@ -8,63 +8,48 @@ struct Race {
     distance: usize,
 }
 
-struct WinCon {
-    hold: usize,
-    run: usize,
-}
-
 impl Race {
-    fn get_win_cons(self: &Self) -> Vec<WinCon> {
-        let mut possible_win_cons = vec![];
-        println!("distance {:?}", self.distance);
+    fn get_win_con_amounts(self: &Self) -> usize {
+        let mut amount: usize = 0;
         for i in 0..=self.time {
-            println!("time {:?}", i);
-            println!("run {:?}", i * (self.time - i));
             if i * (self.time - i) > self.distance {
-                possible_win_cons.push(WinCon {
-                    hold: i,
-                    run: self.time - i,
-                })
+                amount += 1;
             }
         }
-        return possible_win_cons;
+        return amount;
     }
 }
 
-fn get_info_for_each_race(line: &str, info_type: &str) -> Vec<usize> {
+fn get_info_for_race(line: &str, info_type: &str) -> usize {
     line.split_once(info_type)
         .unwrap()
         .1
         .split(" ")
         .collect::<Vec<_>>()
         .iter()
-        .flat_map(|x| x.parse::<usize>())
-        .collect()
+        .filter(|x| x.parse::<usize>().is_ok())
+        .fold("".to_string(), |sum, num_str| sum + &num_str)
+        .parse::<usize>()
+        .unwrap()
 }
 
-fn parse_string_to_race(str: &'static str) -> Vec<Race> {
+fn parse_string_to_race(str: &'static str) -> Race {
     let (time_info_str, distance_info_str) = str.split_once("\n").unwrap();
-    let times = get_info_for_each_race(time_info_str, "Time:");
-    let distances = get_info_for_each_race(distance_info_str, "Distance:");
-    let races: Vec<Race> = times
-        .iter()
-        .enumerate()
-        .map(|(i, time)| Race {
-            time: *time,
-            distance: distances[i],
-        })
-        .collect();
-    return races;
+    let time = get_info_for_race(time_info_str, "Time:");
+    let distance = get_info_for_race(distance_info_str, "Distance:");
+    return Race { time, distance };
 }
 
 fn main() {
-    let races = parse_string_to_race(get_inputs());
-    let mutiply = races
-        .iter()
-        .map(|x| x.get_win_cons().len())
-        .fold(1, |sum, count| {
-            println!("count: {:?}", count);
-            sum * count
-        });
-    println!("mutiply: {:?}", mutiply);
+    let race = parse_string_to_race(get_inputs());
+    // let mutiply = races
+    //     .iter()
+    //     .map(|x| x.get_win_cons().len())
+    //     .fold(1, |sum, count| {
+    //         println!("count: {:?}", count);
+    //         sum * count
+    //     });
+    // println!("mutiply: {:?}", mutiply);
+    let win_con_amount = race.get_win_con_amounts();
+    println!("win_con_amount: {:?}", win_con_amount);
 }
